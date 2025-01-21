b5d9959f97fd9cf59db258f616c56c3ffedd06e8
Patrick You <patrick@data-sleek.com>
2025-01-20 22:59:40 -0800

    Week4_HW_Resubmission

--- bootcamp/materials/4-applying-analytical-patterns/homework/Week4_HW_2_Grouping_Sets.sql
+++ bootcamp/materials/4-applying-analytical-patterns/homework/Week4_HW_2_Grouping_Sets.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS game_details_dashboard;
+-- DROP TABLE IF EXISTS game_details_dashboard;
 
 -- Use GROUPING SETS to create a table that shows the total points scored by each player for each team, each season, and overall.
 CREATE TABLE game_details_dashboard AS
@@ -16,35 +16,56 @@ WITH game_details_augmented AS (
     LEFT JOIN games g 
     ON gd.game_id = g.game_id
     WHERE pts IS NOT NULL
+),
+won_games AS(
+    SELECT 
+        team_abbreviation, 
+        SUM(total_wins) AS total_wins
+    FROM 
+        (SELECT 
+            team_abbreviation,
+            SUM(plus_minus) AS total_plus_minus,
+            COUNT(DISTINCT game_id) AS total_wins
+        FROM game_details
+        GROUP BY game_id, team_abbreviation
+        HAVING SUM(plus_minus) > 0) AS winned_games
+    GROUP BY team_abbreviation
 )
 SELECT
     CASE
-        WHEN GROUPING(player_name) = 0 AND GROUPING(team_abbreviation) = 0 THEN 'player_team'
-        WHEN GROUPING(player_name) = 0 AND GROUPING(season) = 0 THEN 'player_season'
-        WHEN GROUPING(game_id) = 0 AND GROUPING(team_abbreviation) = 0 THEN 'game_team'
+        WHEN GROUPING(gda.player_name) = 0 AND GROUPING(gda.team_abbreviation) = 0 THEN 'player_team' -- Group by player and team
+        WHEN GROUPING(gda.player_name) = 0 AND GROUPING(gda.season) = 0 THEN 'player_season' -- Group by player and season
+        WHEN GROUPING(gda.team_abbreviation) = 0 THEN 'team' -- Group by team
+        ELSE 'overall' -- Overall Grouping
     END AS aggregation_level,
-    COALESCE(player_name, '(overall)') AS player_name,
-    COALESCE(team_abbreviation, '(overall)') AS team_abbreviation,
-    COALESCE(CAST(game_id AS TEXT), '(overall)') AS game_id,
-    COALESCE(CAST(season AS TEXT), '(overall)') AS season,
-    SUM(pts) AS total_points,
-    SUM(plus_minus) AS total_plus_minus
-FROM game_details_augmented
+    COALESCE(gda.player_name, '(overall)') AS player_name,
+    COALESCE(gda.team_abbreviation, '(overall)') AS team_abbreviation,
+    COALESCE(CAST(gda.season AS TEXT), '(overall)') AS season,
+    SUM(gda.pts) AS total_points,
+    MAX(wg.total_wins) AS total_wins
+FROM game_details_augmented gda
+JOIN won_games wg
+ON gda.team_abbreviation = wg.team_abbreviation
 GROUP BY GROUPING SETS (
-    (player_name, team_abbreviation),
-    (player_name, season),
-    (game_id, team_abbreviation)
+    (gda.player_name, gda.team_abbreviation),
+    (gda.player_name, gda.season),
+    (gda.team_abbreviation)
 )
 ORDER BY total_points DESC;
 
 -- Who scored the most points playing for one team?
-SELECT * 
+SELECT
+    player_name,
+    total_points
 FROM game_details_dashboard 
 WHERE aggregation_level = 'player_team' 
 ORDER BY total_points DESC;
 
 -- Who scored the most points in one season?
-SELECT * 
+SELECT
+    player_name,
+    total_points
+    season
 FROM game_details_dashboard 
 WHERE aggregation_level = 'player_season' 
 ORDER BY total_points DESC;
@@ -52,8 +73,6 @@ ORDER BY total_points DESC;
 -- Which team has won the most games?
 SELECT
     team_abbreviation AS team_name,
-    COUNT(DISTINCT game_id) AS total_wins
+    total_wins
 FROM game_details_dashboard 
-WHERE aggregation_level = 'game_team' AND total_plus_minus > 0
-GROUP BY team_abbreviation
-ORDER BY total_wins DESC;
+WHERE aggregation_level = 'team';
--- bootcamp/materials/4-applying-analytical-patterns/homework/Week4_HW_3_Window_Based_Analysis.sql
+++ bootcamp/materials/4-applying-analytical-patterns/homework/Week4_HW_3_Window_Based_Analysis.sql
@@ -35,113 +35,26 @@ LIMIT 1;
 -- How many games in a row did LeBron James score over 10 points a game?
 WITH lebron_games AS (
     SELECT
-        DISTINCT player_name,
-        game_id,
-        pts,
-        CASE 
-            WHEN pts > 10 THEN 1 
-            ELSE 0 
-        END AS scored_above_10
-    FROM game_details
-    WHERE player_name = 'LeBron James'
-),
-streaks AS (
-    SELECT
-        player_name,
-        game_id,
-        pts,
-        scored_above_10,
-        -- Identify streak resets by incrementing a counter when LeBron scores <= 10
-        SUM(CASE 
-                WHEN scored_above_10 = 0 THEN 1 
-                ELSE 0 
-            END) OVER (
-            PARTITION BY player_name
-            ORDER BY game_id
-        ) AS streak_reset
-    FROM lebron_games
-),
-scoring_streaks AS (
-    SELECT
-        player_name,
-        streak_reset,
-        COUNT(*) AS streak_length
-    FROM streaks
-    WHERE scored_above_10 = 1 -- Only consider games where LeBron scored > 10 points
-    GROUP BY player_name, streak_reset
-)
-SELECT 
-    player_name, 
-    MAX(streak_length) AS longest_streak
-FROM scoring_streaks
-GROUP BY player_name;
-
-
-
-
-SELECT * FROM game_details WHERE team_abbreviation = 'LAL' ORDER BY game_id;
-
-with dedupes as (
-select
-    distinct game_id,
-    pts,
-    player_name
-from 
-game_details gd 
-where player_name = 'LeBron James'
-)
-, streak as (select 
-    game_id,
-    case when pts > 10 then 1 
-    when pts <= 10 then 0
-    else null end as pts_over_10,
-    pts,
-    sum(case when pts > 10 then 0 
-    when pts <= 10 then 1
-    else 1 end) over(partition by player_name ORDER BY game_id
-                    ROWS BETWEEN UNBOUNDED PRECEDING AND current row) as streak
-from 
-dedupes gd )
-select
-    *
-from streak
-order by game_id;
-
-
-WITH lebron_games AS (
-    SELECT
         gd.player_name,
-        gd.game_id,
         g.game_date_est,
-        gd.pts,
         CASE 
             WHEN gd.pts > 10 THEN 1 
             ELSE 0 
-        END AS scored_above_10
+        END AS scored_above_10,
+        ROW_NUMBER() OVER (PARTITION BY gd.player_name ORDER BY g.game_date_est) AS row_number
     FROM game_details gd
     JOIN games g
     ON gd.game_id = g.game_id
     WHERE gd.player_name = 'LeBron James'
 ),
-game_numbers AS (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (PARTITION BY player_name ORDER BY game_date_est) AS row_number
-    FROM lebron_games
-),
 streaks AS (
     SELECT
-        game_id,
         player_name,
-        game_date_est,
-        pts,
-        scored_above_10,
-        row_number,
         row_number - SUM(scored_above_10) OVER (
             PARTITION BY player_name
             ORDER BY row_number
         ) AS streak_group
-    FROM game_numbers
+    FROM lebron_games
     WHERE scored_above_10 = 1
 ),
 streak_lengths AS (
